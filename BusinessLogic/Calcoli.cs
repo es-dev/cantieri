bfd9e885f1ac222d38c77d6c1e7072357824bdda
miriam <miriam.iusi@gmail.com>
2015-01-25 18:43:04 +0100

    issue #38

---
+++ BusinessLogic/Calcoli.cs
@@ -0,0 +1,30 @@
+﻿using Library.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Calcoli
+    {
+        public static decimal? GetTotaleFatturaAcquisto(decimal? imponibile, decimal? iva)
+        {
+            try
+            {
+                decimal? totale = 0;
+                if (imponibile != null)
+                    totale += imponibile;
+                if (iva != null)
+                    totale += iva;
+                return totale;
+            }
+            catch (Exception ex)
+            {
+                UtilityError.Write(ex);
+            }
+            return 0;
+        }
+    }
+}
