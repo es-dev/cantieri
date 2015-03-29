a2b5d0fd162de302331a9428782b2e1caf6bd1de
Pasquale Iaquinta <pasqualeiaquinta@hotmail.com>
2015-03-29 20:48:04 +0200

    ppp

--- DataLayer/ESCantieriConnection(2015-03-29 19-00-11).rlinq.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE [FatturaVendita] ADD CONSTRAINT [ref_FatturaVendita_Committente] FOREIGN KEY ([CommittenteId]) REFERENCES [Committente]([Id])
-
-go
-
--- Index 'idx_FatturaVendita_CommittenteId' was not detected in the database. It will be created
-CREATE INDEX [idx_FatturaVendita_CommittenteId] ON [FatturaVendita]([CommittenteId])
-
-go
-
