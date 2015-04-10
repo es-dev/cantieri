039ec1d88c2e809e5eb8cbddaa33cfdb92562869
Pasquale Iaquinta <pasqualeiaquinta@hotmail.com>
2015-04-10 22:39:14 +0200

    EventoDto

---
+++ DataLayer/EventoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class EventoDto
+    {
+        public string Titolo = null;
+        public DateTime Data = DateTime.MinValue;
+        public System.Drawing.Color Color = System.Drawing.Color.Red;
+    }
+}
