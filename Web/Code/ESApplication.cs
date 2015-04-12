fc3d3849dcec664ca2f67d5eda7eed51b9dfb9a6
Pasquale Iaquinta <pasqualeiaquinta@hotmail.com>
2015-04-13 00:59:42 +0200

    refresh

--- Web/Code/ESApplication.cs
+++ Web/Code/ESApplication.cs
@@ -14,7 +14,7 @@ namespace Web.Code
             try
             {
                 view.Title = "ES | GESTIONE CANTIERI";
-                view.Version = "rev. 1.0";
+                view.Version = "rev. 1.0a";
                 view.LogoSoftware = "Images.logoSoftware.png";
                 view.LogoESD = "Images.logoESD.png";
                 view.BackgroundImage = "Images.background.png";
