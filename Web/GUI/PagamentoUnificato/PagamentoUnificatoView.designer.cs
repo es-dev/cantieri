3e44d8bd8c89a3cb39e6d5580da15418717965c2
Miriam Iusi <miriam.iusi@gmail.com>
2015-05-04 22:11:04 +0200

    filtro pagamentounificato

--- Web/GUI/PagamentoUnificato/PagamentoUnificatoView.designer.cs
+++ Web/GUI/PagamentoUnificato/PagamentoUnificatoView.designer.cs
@@ -30,20 +30,180 @@ namespace Web.GUI.PagamentoUnificato
 		/// </summary>
 		private void InitializeComponent()
 		{
+            this.optCodice = new Library.Template.Controls.TemplateEditOption();
+            this.optData = new Library.Template.Controls.TemplateEditOption();
+            this.editFornitore = new Library.Template.Controls.TemplateEditCombo();
+            this.editDataInizio = new Library.Template.Controls.TemplateEditDate();
+            this.editDataFine = new Library.Template.Controls.TemplateEditDate();
             this.panelCommands.SuspendLayout();
+            this.panelAdvancedSearch.SuspendLayout();
+            this.panelOrderBy.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnAdd
+            // panelAdvancedSearch
             // 
+            this.panelAdvancedSearch.Controls.Add(this.editDataFine);
+            this.panelAdvancedSearch.Controls.Add(this.editDataInizio);
+            this.panelAdvancedSearch.Controls.Add(this.editFornitore);
+            this.panelAdvancedSearch.Location = new System.Drawing.Point(244, 68);
+            this.panelAdvancedSearch.Size = new System.Drawing.Size(622, 249);
+            this.panelAdvancedSearch.Controls.SetChildIndex(this.btnConfirmAdvancedSearch, 0);
+            this.panelAdvancedSearch.Controls.SetChildIndex(this.btnCloseAdvancedSearch, 0);
+            this.panelAdvancedSearch.Controls.SetChildIndex(this.btnCancelAdvancedSearch, 0);
+            this.panelAdvancedSearch.Controls.SetChildIndex(this.lblTitleAdvancedSearch, 0);
+            this.panelAdvancedSearch.Controls.SetChildIndex(this.editFornitore, 0);
+            this.panelAdvancedSearch.Controls.SetChildIndex(this.editDataInizio, 0);
+            this.panelAdvancedSearch.Controls.SetChildIndex(this.editDataFine, 0);
+            // 
+            // panelOrderBy
+            // 
+            this.panelOrderBy.Controls.Add(this.optData);
+            this.panelOrderBy.Controls.Add(this.optCodice);
+            this.panelOrderBy.Controls.SetChildIndex(this.btnConfirmOrderBy, 0);
+            this.panelOrderBy.Controls.SetChildIndex(this.btnCloseOrderBy, 0);
+            this.panelOrderBy.Controls.SetChildIndex(this.btnCancelOrderBy, 0);
+            this.panelOrderBy.Controls.SetChildIndex(this.lblTitleOrderBy, 0);
+            this.panelOrderBy.Controls.SetChildIndex(this.optAscending, 0);
+            this.panelOrderBy.Controls.SetChildIndex(this.optDescending, 0);
+            this.panelOrderBy.Controls.SetChildIndex(this.optCodice, 0);
+            this.panelOrderBy.Controls.SetChildIndex(this.optData, 0);
+            // 
+            // btnCloseAdvancedSearch
+            // 
+            this.btnCloseAdvancedSearch.Location = new System.Drawing.Point(592, 2);
+            // 
+            // btnConfirmAdvancedSearch
+            // 
+            this.btnConfirmAdvancedSearch.Location = new System.Drawing.Point(487, 208);
+            // 
+            // btnCancelAdvancedSearch
+            // 
+            this.btnCancelAdvancedSearch.Location = new System.Drawing.Point(321, 208);
+            // 
+            // lblTitleAdvancedSearch
+            // 
+            this.lblTitleAdvancedSearch.Size = new System.Drawing.Size(584, 44);
+            // 
+            // optCodice
+            // 
+            this.optCodice.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.optCodice.BackColor = System.Drawing.Color.Transparent;
+            this.optCodice.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.optCodice.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.optCodice.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.optCodice.Changed = true;
+            this.optCodice.Editing = true;
+            this.optCodice.Group = "Option1";
+            this.optCodice.Label = "";
+            this.optCodice.LabelWidth = 0;
+            this.optCodice.Location = new System.Drawing.Point(19, 59);
+            this.optCodice.Name = "optCodice";
+            this.optCodice.ReadOnly = false;
+            this.optCodice.Required = false;
+            this.optCodice.Size = new System.Drawing.Size(380, 30);
+            this.optCodice.TabIndex = 2;
+            this.optCodice.Text = "Codice";
+            this.optCodice.Value = false;
+            // 
+            // optData
+            // 
+            this.optData.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.optData.BackColor = System.Drawing.Color.Transparent;
+            this.optData.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.optData.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.optData.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.optData.Changed = true;
+            this.optData.Editing = true;
+            this.optData.Group = "Option1";
+            this.optData.Label = "";
+            this.optData.LabelWidth = 0;
+            this.optData.Location = new System.Drawing.Point(19, 106);
+            this.optData.Name = "optData";
+            this.optData.ReadOnly = false;
+            this.optData.Required = false;
+            this.optData.Size = new System.Drawing.Size(380, 30);
+            this.optData.TabIndex = 2;
+            this.optData.Text = "Data";
+            this.optData.Value = false;
+            // 
+            // editFornitore
+            // 
+            this.editFornitore.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editFornitore.BackColor = System.Drawing.Color.Transparent;
+            this.editFornitore.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editFornitore.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editFornitore.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editFornitore.Changed = false;
+            this.editFornitore.Editing = true;
+            this.editFornitore.Label = "Fornitore";
+            this.editFornitore.LabelWidth = 135;
+            this.editFornitore.Location = new System.Drawing.Point(19, 60);
+            this.editFornitore.Model = null;
+            this.editFornitore.Name = "editFornitore";
+            this.editFornitore.ReadOnly = false;
+            this.editFornitore.Required = false;
+            this.editFornitore.Size = new System.Drawing.Size(572, 30);
+            this.editFornitore.TabIndex = 7;
+            this.editFornitore.Value = null;
+            this.editFornitore.ComboConfirm += new Library.Template.Controls.TemplateEditCombo.ComboConfirmHanlder(this.editFornitore_ComboConfirm);
+            this.editFornitore.ComboClick += new Library.Template.Controls.TemplateEditCombo.ComboClickHandler(this.editFornitore_ComboClick);
+            // 
+            // editDataInizio
+            // 
+            this.editDataInizio.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editDataInizio.BackColor = System.Drawing.Color.Transparent;
+            this.editDataInizio.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editDataInizio.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editDataInizio.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editDataInizio.Changed = false;
+            this.editDataInizio.Editing = true;
+            this.editDataInizio.Label = "Data dal";
+            this.editDataInizio.LabelWidth = 135;
+            this.editDataInizio.Location = new System.Drawing.Point(19, 106);
+            this.editDataInizio.Name = "editDataInizio";
+            this.editDataInizio.ReadOnly = false;
+            this.editDataInizio.Required = false;
+            this.editDataInizio.Size = new System.Drawing.Size(572, 30);
+            this.editDataInizio.TabIndex = 5;
+            this.editDataInizio.Value = null;
+            // 
+            // editDataFine
+            // 
+            this.editDataFine.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editDataFine.BackColor = System.Drawing.Color.Transparent;
+            this.editDataFine.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editDataFine.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editDataFine.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editDataFine.Changed = false;
+            this.editDataFine.Editing = true;
+            this.editDataFine.Label = "Data al";
+            this.editDataFine.LabelWidth = 135;
+            this.editDataFine.Location = new System.Drawing.Point(19, 152);
+            this.editDataFine.Name = "editDataFine";
+            this.editDataFine.ReadOnly = false;
+            this.editDataFine.Required = false;
+            this.editDataFine.Size = new System.Drawing.Size(572, 30);
+            this.editDataFine.TabIndex = 7;
+            this.editDataFine.Value = null;
+            this.Controls.SetChildIndex(this.panelAdvancedSearch, 0);
+            this.Controls.SetChildIndex(this.panelOrderBy, 0);
             this.Controls.SetChildIndex(this.panelCommands, 0);
             this.Controls.SetChildIndex(this.txtSearch, 0);
             this.Controls.SetChildIndex(this.btnOrderBy, 0);
             this.Controls.SetChildIndex(this.btnAdvancedSearch, 0);
             this.panelCommands.ResumeLayout(false);
+            this.panelAdvancedSearch.ResumeLayout(false);
+            this.panelOrderBy.ResumeLayout(false);
             this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+        private Library.Template.Controls.TemplateEditOption optData;
+        private Library.Template.Controls.TemplateEditOption optCodice;
+        private Library.Template.Controls.TemplateEditDate editDataFine;
+        private Library.Template.Controls.TemplateEditDate editDataInizio;
+        private Library.Template.Controls.TemplateEditCombo editFornitore;
 	}
 }
